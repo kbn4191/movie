073105156fb6c94c77507bce40fba751ecbc77f8
kbn4191 <kaluboniface@yahoo.com>
2023-11-12 00:37:25 +0100

    try

--- src/app/component/nav/nav.tsx
+++ src/app/component/nav/nav.tsx
@@ -27,11 +27,8 @@ export default function Navigation() {
     }
   };
 
-<<<<<<< HEAD
 //window.addEventListener("scroll", chnageNavBagckground);
-=======
   //window.addEventListener("scroll", chnageNavBagckground);
->>>>>>> e18640b4322e0cd8217858ed461b6437eb154ed0
 
   return (
     <>
