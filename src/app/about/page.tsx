96957e8d00e8d854467583f2bf42296bdb8f7507
kbn4191 <kaluboniface@yahoo.com>
2023-11-12 17:45:04 +0100

    about update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import { Box, Container, Typography, Grid } from "@mui/material";
 import AboutHeader from "../component/aboutHeader/aboutHeader";
+import AboutBody from "../component/aboutHeader/aboutBody";
 export default function About() {
 
     return(
         <>
         <Box>
             <AboutHeader/>
+            <AboutBody/>
            
             
             </Box>
