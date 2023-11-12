471ba0e9ab88b1fcc2b985759d09ca9fb7e31e5f
kbn4191 <kaluboniface@yahoo.com>
2023-11-12 20:40:16 +0100

    accordian fixed

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6ea6ae0acdebd80d")
+/******/ 		__webpack_require__.h = () => ("527e60fcf822a1ae")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ src/app/component/aboutHeader/abouCvData.ts
@@ -0,0 +1,41 @@
+type CV = {
+    id: number;
+    Portfolio: String;
+    Description: String
+}
+
+export const CV = [
+    {
+        id:1,
+        Portfolio:"TRAINING",
+        Description:"National Theatre School of Canada, Montréal, Québec Set design, graduated class of 1998",
+        
+    },
+    {
+        id:2,
+        Portfolio: "AWARDS",
+        Description: `Best Production Design, “Château Laurier” Web series, Geneva Film Co.Produced and Directed by James Stewart
+        Sao Paulo Webfest 2018, Sao Paulo, Brasil
+        2002 Prix de l’Excellence – Franco-Ontarian theatre artist of the yearThéâtre Action`,
+        
+    },
+    {
+        id:3,
+        Portfolio:"NOMINATIONS",
+        Description:`Best Production Design, “Château Laurier” Web series, Geneva Film Co.Produced and Directed by James 
+        StewartInternational Academy of Web Television 2018 Los Angeles, USA 
+        
+        Best Production Design “Château Laurier” 
+        Web series, Geneva Film Co.Produced and Directed by James Stewart Minnesota WebFest 2019 Minnesota, 
+        USA  
+        
+        Best Production Design or Art Direction in a Non-Dramatic Program or Series “Inspire Awards 2012” Canadian 
+        Screen Awards nomination - 2013  
+        
+        Best Production Design or Art Direction in a Non-Dramatic Program or Series
+         “National Aboriginal Achievement Awards 2007” Gemini nomination - 2007`,
+        
+        
+    },
+  
+]
