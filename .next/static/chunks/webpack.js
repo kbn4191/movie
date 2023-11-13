05c10860b59bc27699463ea53aab9e7d8b5d3b3c
kbn4191 <kaluboniface@yahoo.com>
2023-11-13 23:00:44 +0100

    image fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc512fb709070fc0"; }
+/******/ 		__webpack_require__.h = function() { return "1495fd1619ae3038"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
